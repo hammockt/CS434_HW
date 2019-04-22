9626f6157fd71e657b5905b97e409fd24b2c03cf
Charles Koll <charles@kollonline.com>
2019-04-22 14:29:53 -0700

    Add test boundary retrieval

--- HW2/q2_1.py
+++ HW2/q2_1.py
@@ -4,6 +4,7 @@ implementation for part2
 
 import sys
 import math
+import statistics
 import numpy
 
 numpy.set_printoptions(suppress=True)
@@ -11,20 +12,43 @@ numpy.set_printoptions(suppress=True)
 #normalize(x_i) = (x_i - min(x)) / range(x)
 #gets the min and range for each column and uses that to normalize the column
 def normalize_columns(matrix):
-    """ normalizes all of the columns in the given matrix to [0,1] """
+    """ Normalizes all of the columns in the given matrix to [0,1] """
     return (matrix - matrix.min(0)) / matrix.ptp(0)
 
 def total_wrong(expected_y, y):
-    """ gives the percentage of wrong guesses in expected_y """
+    """ Gives the percentage of wrong guesses in expected_y """
     diff = expected_y - y
     num_wrong = len([value for value in diff if not math.isclose(value, 0.0)])
     return num_wrong / len(y)
 
+
+class Tree():
+    def __init__(self, root):
+        self.root = root
+        self.height = self.get_height()
+
+    def get_height(self, node_param=None):
+        if node_param is None:
+            node = self.root
+        else:
+            node = node_param
+        if not node.children:
+            return 0
+        return 1 + max(self.get_height(node.children[0]),
+                       self.get_height(node.children[1]))
+
+    def predicted_value(self, point):
+        return self.root.predicted_value(point)
+
+
 class Node():
-    def __init__(self, x_vals, y_vals):
+    def __init__(self, x_vals, y_vals, parent=None):
+        self.parent = parent
         self.children = []
         self.x_vals = x_vals
         self.y_vals = y_vals
+        self.feature = None
+        self.test_bound = None
 
     #https://stackoverflow.com/a/1859910
     def entropy(self):
@@ -35,8 +59,7 @@ class Node():
             key = repr(y_val)
             if key not in y_counts:
                 y_counts[key] = 0
-            else:
-                y_counts[key] += 1
+            y_counts[key] += 1
         total_y_count = sum(y_counts.values())
 
         #we should probably break this into two separate methods...ehh
@@ -50,6 +73,66 @@ class Node():
                 u_s += len(child.y_vals) / total_y_count * child.entropy()
 
         return u_s
+
+    def test(self):
+        best_feature = None
+        for col in range(len(self.x_vals[0])):
+            # Get column from x_vals
+            feature_vals = [
+                (y_index, self.x_vals[y_index][col])
+                for y_index in range(len(self.y_vals))
+            ]
+            feature_vals.sort(key=lambda x: x[1])
+            for index, val in enumerate(feature_vals[1:]):
+                # Since we're skipping the first element
+                j = index + 1
+                # <, >=
+                # Set y_val separations based on test boundary
+                y_vals = {"l": [self.y_vals[k[0]] for k in feature_vals[:j]],
+                          "r": [self.y_vals[k[0]] for k in feature_vals[j:]]}
+
+                self.children.append(Node(None, y_vals["l"], self))
+                self.children.append(Node(None, y_vals["r"], self))
+                entropy = self.entropy()
+                if best_feature is None or best_feature["entropy"] > entropy:
+                    best_feature = {
+                        "feature_index": col,
+                        "test_bound": val[1],
+                        "entropy": entropy
+                    }
+                self.children = []
+        self.feature = best_feature["feature_index"]
+        self.test_bound = best_feature["test_bound"]
+        return best_feature
+
+    def test_and_apply(self):
+        best_feature = self.test()
+        x_vals = ([], [])
+        y_vals = ([], [])
+        for i, point in enumerate(self.x_vals):
+            if point[self.feature] < self.test_bound:
+                x_vals[0].append(point)
+                y_vals[0].append(self.y_vals[i])
+            else:
+                x_vals[1].append(point)
+                y_vals[1].append(self.y_vals[i])
+        for i, x_val in enumerate(x_vals):
+            self.children.append(Node(x_val, y_vals[i], self))
+        return best_feature
+
+    def predicted_value(self, point):
+        if not self.children:
+            return self.decision()
+        if point[self.feature] < self.test_bound:
+            return self.children[0].predicted_value(point)
+        return self.children[1].predicted_value(point)
+
+    def decision(self):
+        try:
+            return statistics.mode(self.y_vals)
+        except StatisticsError:
+            return self.y_vals[0]
+
 
 def main():
     """ entry point """
@@ -81,15 +164,20 @@ def main():
 
     basic_node = Node(training_x, training_y)
     entropy_before = basic_node.entropy()
-    print(f"entropy of root: {entropy_before}")
 
-    #basic 50/50 split
-    basic_node.children.append(Node(training_x[:len(training_x)//2], training_y[:len(training_y)//2]))
-    basic_node.children.append(Node(training_x[len(training_x)//2:], training_y[len(training_y)//2:]))
-    entropy_after = basic_node.entropy()
-    print(f"entropy of basic split: {entropy_after}")
-    print(f"information gain: {entropy_before - entropy_after}")
-    print(f"entropy of child 0: {basic_node.children[0].entropy()}")
-    print(f"entropy of child 1: {basic_node.children[1].entropy()}")
+    test_info = basic_node.test()
+    feature_index = test_info["feature_index"]
+    test_bound = test_info["test_bound"]
+    entropy_after = test_info["entropy"]
+    info_gain = entropy_before - entropy_after
+
+    print(f"Feature index of test (decision stump): {feature_index}")
+    print(f"Test boundary (decision stump):         {test_bound}")
+    print(f"Information gain:                       {info_gain}")
+
+    expected_training_y = [basic_node.predicted_value(point) for point in training_x]
+    expected_test_y = [basic_node.predicted_value(point) for point in test_x]
+    print("Training error:      {0:.2f}".format(total_wrong(expected_training_y, training_y)))
+    print("Testing error:       {0:.2f}".format(total_wrong(expected_test_y, test_y)))
 
 main()
